363c3fade4d3f0c7c4ecbac6ef39160d5aa009d8
JamesSBeedle <jamessbeedle@gmail.com>
2021-03-06 15:14:41 +0000

    added form to app.vue, imported userData into form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <emissions-form></emissions-form>
     
   </div>
 </template>
 
 <script>
+import emissionsForm from '@/components/emissionsForm.vue';
 
 
 export default {
   name: 'App',
   components: {
+    'emissions-form' : emissionsForm
   
   }
 }
