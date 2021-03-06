8de37d09e222e628dc048dbc48be027958aabe35
DavidTaylor96 <david_taylor96@outlook.com>
2021-03-06 15:34:49 +0000

    pieChart

--- src/components/emissionGrid.vue
+++ src/components/emissionGrid.vue
@@ -1,13 +1,45 @@
 <template>
-  <h2>This will be the grid/pie chart</h2>
+  <div>
+    <h2>This will be the grid/pie chart</h2>
+      <GChart
+        :type="PieChart"
+        :data="chartData"
+        :options="chartOptions"
+      />
+  </div>
 </template>
 
 <script>
 import EmissionsForm from './emissionsForm.vue'
 import { eventBus } from '@/main.js'
-import { Chart } from 'chart.js'
+import { GChart } from 'vue-google-charts'
 
 export default {
+  name: 'emissions-chart',
+  components:{ GChart },
+  data(){
+    return {
+      chartData:[
+        ['transport', 'energy', 'food']
+        [20, 0.2, 2.1]
+      ],
+      chartOptions:{
+        width: 800,
+        height: 240,
+        title: 'Emissions User Data',
+      },
+    }
+  },
+  props:['userData'],
+
+  // computed: {
+  //   chartData: function () {
+  //     if(this.userData){
+  //       const chartData = [['emissions'], ...this.userData.map(mix => ([user.emissions]))]
+  //       return chartData
+  //     }
+  //   }
+  // }
 
 }
 </script>
