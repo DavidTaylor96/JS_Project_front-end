13c16de604be8e56967d0d490892d2c27dff1ab3
JamesSBeedle <jamessbeedle@gmail.com>
2021-03-06 15:30:50 +0000

    added emissions-chart import to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <emissions-form></emissions-form>
+    <emissions-chart></emissions-chart>
     
   </div>
 </template>
 
 <script>
 import emissionsForm from '@/components/emissionsForm.vue';
+import emissionGrid from '@/components/emissionGrid.vue';
 
 
 export default {
   name: 'App',
   components: {
-    'emissions-form' : emissionsForm
+    'emissions-form' : emissionsForm,
+    'emissons-chart' : emissionGrid
   
   }
 }
