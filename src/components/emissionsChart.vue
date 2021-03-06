0902cba293201aa806de18f4420404fc73d2839a
DavidTaylor96 <david_taylor96@outlook.com>
2021-03-06 19:11:16 +0000

    not working yet

--- src/components/emissionsChart.vue
+++ src/components/emissionsChart.vue
@@ -1,11 +1,11 @@
-<template>
+<!---<template>
   <div>
     <h2>This will be the grid/pie chart</h2>
-      <GChart
-        :type="PieChart"
-        :data="chartData"
-        :options="chartOptions"
-      />
+    <GChart
+     type="PieChart"
+     :data="chartData"
+     :options="chartOptions"
+    />
   </div>
 </template>
 
@@ -20,14 +20,18 @@ export default {
   data(){
     return {
       chartData:[
-        ['transport', 'energy', 'food']
-        [20, 0.2, 2.1]
+        ['Co2 used', 'Pre-Day'],
+        ['Transport', 20],
+        ['Energy', 60],
+        ['Food', 40]
       ],
       chartOptions:{
-        width: 800,
-        height: 240,
-        title: 'Emissions User Data',
-      },
+        chart: {
+          width: 800,
+          height: 240,
+          title: 'Emissions User Data',
+        },
+      }
     }
   },
   props:['userData'],
@@ -46,4 +50,35 @@ export default {
 
 <style>
 
-</style>
+</style> -->
+
+<template>
+  <radial-progress-bar 
+     :diameter="200"
+     :transport-traveled="Transport"
+     :energy-used="Energy"
+     :food-eaten="Food">
+   <p>Transport: {{ Transport }}kg</p>
+   <p>Energy: {{ Energy }}kg</p>
+   <p>Food: {{ Food }}kg</p>
+  </radial-progress-bar>
+</template>
+ 
+<script>
+import RadialProgressBar from 'vue-radial-progress'
+ 
+export default {
+  data () {
+    return {
+      Transport: 10,
+      Energy: 5,
+      Food: 0.2,
+    }
+  },
+ 
+  components: {
+    RadialProgressBar
+  }
+}
+</script>
+
