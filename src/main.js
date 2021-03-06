89a851343c1853b020f1d7eb42e9cf8d7a3280f8
JamesSBeedle <jamessbeedle@gmail.com>
2021-03-06 16:36:39 +0000

    modifies main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-
 
 Vue.config.productionTip = false
 
-
-=======
-
-Vue.config.productionTip = false
-
->>>>>>> 1144870418737b943cdbb34271e67275105901c4
 
 export const eventBus = new Vue();
 
