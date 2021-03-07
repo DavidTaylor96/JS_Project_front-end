60169cf09d02816910707e4ed76e77d7279e12be
Jan Metelski <janekmetelski@hotmail.com>
2021-03-07 13:02:46 +0000

    Some modifications to grid

--- src/components/emissionGrid.vue
+++ src/components/emissionGrid.vue
@@ -1,9 +1,24 @@
 <template>
-  
+  <article>
+      <emissions-details></emissions-details>
+  </article>
 </template>
 
 <script>
+import {eventBus} from '@/main.js'
+import emissionsDetails from './emissionsDetails.vue'
+import userData from '@/services/userData'
+
 export default {
+    data () {
+        return {
+
+        };
+    },
+  components: { 
+      'emission-details' : emissionsDetails 
+
+      },
 
 }
 </script>
