ec1582086e047de4a332a21f0888e4cd5077354f
DavidTaylor96 <david_taylor96@outlook.com>
2021-03-08 14:52:15 +0000

    getting there

--- src/components/emissionsChart.vue
+++ src/components/emissionsChart.vue
@@ -1,43 +1,67 @@
 <template>
-  <div id="app">
-      <vue-circle
-        ref="co2produced"
-        :co2Emitted="userData"
-        :size="150"
-        :scale="654"
-        insert-mode="append"
-        @vue-circle-progress="progress"
-        >
-        <p id="tag" >Co2 Emittied</p>
-      </vue-circle>
+  <div class="progress-bar">
+    <radial-progress-bar
+        :diameter="170"
+        :completedSteps="co2Emitted"
+        :totalSteps="maxCo2Produced"
+        :startColor="startColor"
+        :stopColor="stopColor"
+        :innerStrokeColor="innerStrokeColor"
+        :animateSpeed="animateSpeed"
+        :timingFunc="timingFunc">
+    <div class="inner-text">
+      <h3>{{ maxCo2Produced }}kg</h3>
+      <p id="inner-texts">Max per day</p>
+    </div>
+    <div class="co2-emitted">
+      <h3>{{ co2Emitted }}kg</h3>
+      <p id="inner-texts">Co2 Emitted</p>
+    </div>
+    </radial-progress-bar>
   </div>
 </template>
-
+ 
 <script>
- import VueCircle from '@/components/emissionsProgress.vue'
-  export default {
-    components: {
-      VueCircle
-    },
-    data(){
-      return{
-        fill : { gradient: ["green"] },
-        userData: 200
+import RadialProgressBar from 'vue-radial-progress'
+ 
+export default {
+  data () {
+    return {
+      co2Emitted : 10,
+      maxCo2Produced: 22,
+      startColor: "lightgreen",
+      stopColor: "green",
+      innerStrokeColor: "lightgray",
+      animateSpeed: 1000,
+      timingFunc: "liner"
+    }
+  },
+  props: [],
+  methods:{
+    progress(progress){
+      if (progress > 22) {
+        return this.startColor = "red"
       }
-    },
-    methods:{
-      progress(progress){
-        if (progress > 654) {
-          this.$refs.co2produced.updateFill("red");
-        }
-      }
-    },
+    }
+  },
+  components: {
+    RadialProgressBar
   }
+}
 </script>
 <style scoped>
-#tag{
-  margin: 0;
-  font-size: 13px;
+.progress-bar{
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  font-size: 14px;
+}
+.inner-text{
+  padding: 0px;
+  line-height: 0.1em;
 }
 
+.co2-emitted{
+  line-height: 0.1em;
+}
 </style>
